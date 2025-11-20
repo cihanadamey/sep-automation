6a58ee3a4007b8cd93a38cb8a71e65dd030964a5
Cihan Adamey <cihanadamey1@gmail.com>
2025-11-20 12:05:19 -0700

    feature-19

--- steps/getting-started/clickNextButtonStep1.js
+++ steps/getting-started/clickNextButtonStep1.js
@@ -3,3 +3,19 @@ import { expect} from "@playwright/test";
 import { startApplicationPage, page } from "../../globalPagesSetup.js";
 import { productInfo } from "../../utilities/qa-data-reader.js";
 
+
+
+Then('user click on the next button.', async function () {
+
+    await startApplicationPage.nextButton.click();
+
+});
+
+Then('user should be on the next page.', async function () {
+
+    await startApplicationPage.paymentPlanStepCircle.isVisible();
+
+    await expect(startApplicationPage.paymentPlanText).toContainText("Payment plan");
+
+});
+
